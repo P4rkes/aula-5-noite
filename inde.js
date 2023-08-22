beb7b687ec69c563330a1e943bfe83ac709fd9b2
P4rkes <79593921+P4rkes@users.noreply.github.com>
2023-08-21 21:43:32 -0300

    Create index.js

---
+++ inde.js
@@ -0,0 +1,34 @@
+/*
+* Parkes
+* eu venderia todos os ossos do meu corpo por pedras de ametista porque roxo é sua cor favorita
+*/
+console.log("masqueico");
+//prompt("digite seu nome");
+
+//Variaveis
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log(variavelB);
+
+let numero = 50;
+console.log(numero);
+numero = 80;
+console.log(numero);
+
+
+
+const nomeanterior = "Parkes"
+const idadeanterior = 17
+
+
+
+
+//variaveis2
+
+const nome = "Luke";
+const sobrenome = "S parkes";
+const idade = 17;
+
+let estudante = true;
+
+console.log("Olá", "meu nome é:", nome, sobrenome, ", eu tenho",idade, "anos.", "se sou um estudante? ", estudante);
